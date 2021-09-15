1feb1c68982c3bacd4f9fc2566b8ed71e501a600
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-09-15 11:31:43 +0200

    bug: update some typings

--- webgl/assets/utils/buildHelpers.ts
+++ webgl/assets/utils/buildHelpers.ts
@@ -1,11 +1,11 @@
 import * as THREE from 'three';
-import { Sizes, Position } from '../../types';
+import { ISize, IPosition } from '../../types';
 
 export const buildScene = (): THREE.Scene => {
     return new THREE.Scene();
 };
 
-export const buildRenderer = (canvas: HTMLCanvasElement , sizes: Sizes): THREE.WebGLRenderer => {
+export const buildRenderer = (canvas: HTMLCanvasElement , sizes: ISize): THREE.WebGLRenderer => {
     const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ canvas: canvas });
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -13,7 +13,7 @@ export const buildRenderer = (canvas: HTMLCanvasElement , sizes: Sizes): THREE.W
     return renderer;
 };
 
-export const buildCamera = (scene: THREE.Scene, sizes: Sizes, position: Position): THREE.PerspectiveCamera => {
+export const buildCamera = (scene: THREE.Scene, sizes: ISize, position: IPosition): THREE.PerspectiveCamera => {
     const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
     camera.position.set(position.x, position.y, position.z);
     camera.lookAt(new THREE.Vector3),
