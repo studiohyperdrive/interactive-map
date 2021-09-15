863fc308ceb11063ce3bf207ee72796a6c1f4ba9
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-09-15 13:22:21 +0200

    bug: function scoping

--- webgl/three-entry-point.ts
+++ webgl/three-entry-point.ts
@@ -13,21 +13,21 @@ export default class ThreeEntryPoint {
 	}
 
 	public bindEventListeners(): void {
-		window.onresize = this.resizeCanvas;
-		
+		window.onresize = () => {this.resizeCanvas()};
+
 		this.resizeCanvas();
 	}
 
-	public resizeCanvas(): void {		
+	public resizeCanvas(): void {
 		this.canvas.style.width = '100%';
-		this.canvas.style.height= '100%';
+		this.canvas.style.height = '100%';
 		this.canvas.width = this.canvas.offsetWidth;
 		this.canvas.height = this.canvas.offsetHeight;
 		this.manager.onWindowResizeCallback();
 	}
 
 	public render(): void {
-		requestAnimationFrame(() => {this.render()});
+		requestAnimationFrame(() => { this.render() });
 		this.manager.update();
 	};
 };
