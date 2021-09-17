ed8d723b2f6ba2c49d320cef9993b32091c4812d
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2021-09-17 11:20:57 +0200

    feat: move hover config to separate file

--- components/webgl/webgl.tsx
+++ components/webgl/webgl.tsx
@@ -2,9 +2,9 @@ import React, { useEffect, useRef } from 'react';
 
 import ThreeEntryPoint from '../../webgl/three-entry-point';
 import click from '../../bindings/click';
+import hover from '../../bindings/hover';
 
 import { WebGLProps } from './webgl.types';
-import { Mesh, MeshPhysicalMaterial } from 'three';
 
 const WebGL: React.FC<WebGLProps> = () => {
 	const threeRootElement = useRef<HTMLCanvasElement | null>(null);
@@ -14,44 +14,7 @@ const WebGL: React.FC<WebGLProps> = () => {
 			new ThreeEntryPoint(
 				threeRootElement.current,
 				click,
-				[
-					{
-						name: 'skyscraper',
-						matching: 'partial',
-						onHoverStart: (mesh: Mesh) => {
-							const random = new MeshPhysicalMaterial({ color: Math.random() * 0xffffff });
-							mesh.material = random;
-						},
-						onHoverEnd: (mesh: Mesh) => {
-							const random = new MeshPhysicalMaterial({ 
-								color: 0xE7E7E7,
-								metalness: 0.173,
-								specularIntensity: 0.5,
-								roughness: 0.5,
-								clearcoatRoughness: 0.03,
-							});
-							mesh.material = random;
-						},
-					},
-					{
-						name: 'small-house',
-						matching: 'partial',
-						onHoverStart: (mesh: Mesh) => {
-							const random = new MeshPhysicalMaterial({ color: Math.random() * 0xffffff });
-							mesh.material = random;
-						},
-						onHoverEnd: (mesh: Mesh) => {
-							const random = new MeshPhysicalMaterial({ 
-								color: 0xE7E7E7,
-								metalness: 0.173,
-								specularIntensity: 0.5,
-								roughness: 0.5,
-								clearcoatRoughness: 0.03,
-							});
-							mesh.material = random;
-						},
-					}
-				]
+				hover,
 			);
 		}
 	}, []);
