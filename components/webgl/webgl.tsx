e94d33fa8688f91bd1a25482cd450b8e606834ba
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-09-20 16:06:16 +0200

    feat: connect webgl to state

--- components/webgl/webgl.tsx
+++ components/webgl/webgl.tsx
@@ -1,55 +1,58 @@
-import React, { useEffect, useRef } from "react";
+import { FC, useEffect, useRef } from "react";
+import { connect } from "react-redux";
 import { useRouter } from "next/dist/client/router";
 
 import ThreeEntryPoint from "../../webgl/three-entry-point";
 import createClickBindings from "../../bindings/click";
-import hover from "../../bindings/hover";
+import createHoverBindings from "../../bindings/hover";
 
 import actions from "../../redux/actions";
 import store from "../../redux/store";
 
 import { WebGLProps } from "./webgl.types";
 
-const WebGL: React.FC<WebGLProps> = ({three}) => {
-	const threeRootElement = useRef<HTMLCanvasElement | null>(null);
-	const router = useRouter();
+const WebGL: FC<WebGLProps> = ({ three }) => {
+  const threeRootElement = useRef<HTMLCanvasElement | null>(null);
+  const router = useRouter();
 
-	useEffect(() => {
-		if (threeRootElement.current) {
-			if (three === undefined) {
-				store.dispatch({
-					type: actions.three.set,
-					payload: new ThreeEntryPoint(
-						threeRootElement.current,
-						createClickBindings(router),
-						hover,
-					)
-				});
-			} else {
-				threeRootElement.current?.replaceWith(three.canvas);
-			}
-		}
-	}, []);
+  useEffect(() => {
+    if (threeRootElement.current) {
+      if (three === undefined) {
+        store.dispatch({
+          type: actions.three.set,
+          payload: new ThreeEntryPoint(
+            threeRootElement.current,
+            createClickBindings(router),
+            createHoverBindings(store),
+          )
+        });
+      } else {
+        threeRootElement.current?.replaceWith(three.canvas);
+      }
+    }
+  }, []);
 
-	return (
-        <div>
-		    <canvas ref={threeRootElement} />
+  return (
+    <div>
+      <canvas ref={threeRootElement} />
 
-            <div className="webgl__rotate-buttons">
-                <div className="webgl__rotate-button" onClick={() => {
-					const manager = store.getState().three?.manager;
-					manager?.controls?.handleClickRotateLeft();
-					manager?.update();
-				}}>{"<-"}</div>
-				
-                <div className="webgl__rotate-button" onClick={() => {
-					const manager = store.getState().three?.manager;
-					manager?.controls?.handleClickRotateRight();
-					manager?.update();
-				}}>{"->"}</div>
-            </div>
-        </div>
-	);
+      <div className="webgl__rotate-buttons">
+        <div className="webgl__rotate-button" onClick={() => {
+          const manager = store.getState().three?.manager;
+          manager?.controls?.handleClickRotateLeft();
+          manager?.update();
+        }}>{"<-"}</div>
+
+        <div className="webgl__rotate-button" onClick={() => {
+          const manager = store.getState().three?.manager;
+          manager?.controls?.handleClickRotateRight();
+          manager?.update();
+        }}>{"->"}</div>
+      </div>
+    </div>
+  );
 };
 
-export default WebGL;
+export default connect((state: {three?: ThreeEntryPoint}) => {
+  return {three: state.three};
+})(WebGL);
