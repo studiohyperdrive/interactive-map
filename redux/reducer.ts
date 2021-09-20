51fd023adb25878246cb109b4d950eef5d02dac2
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-09-20 16:13:14 +0200

    chore: remove reducer logger

--- redux/reducer.ts
+++ redux/reducer.ts
@@ -8,8 +8,6 @@ const initialState = {
 }
 
 export default function reducer(state = initialState, action: AnyAction) {
-  console.info('reducer', action);
-  
   switch (action.type) {
     case actions.three.set: {
       return { ...state, three: action.payload }
