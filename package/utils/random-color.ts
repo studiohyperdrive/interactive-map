c2c2206d077ed2fb29a120ece9c47533e9d3197a
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2021-10-04 13:38:15 +0200

    feat: clone random color materials

--- package/utils/random-color.ts
+++ package/utils/random-color.ts
@@ -1,5 +1,8 @@
-import { Mesh, MeshPhysicalMaterial } from "three";
+import { Mesh, MeshPhysicalMaterial, MeshStandardMaterial } from "three";
 
 export const mutateRandomColor = (mesh: Mesh): void => {
     mesh.material = new MeshPhysicalMaterial({ color: Math.random() * 0xffffff });
+    const material = (mesh.material as MeshStandardMaterial).clone();    
+    material.color.setHex(Math.random() * 0xffffff);
+    mesh.material = material;
 };
