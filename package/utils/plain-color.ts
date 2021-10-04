a59ef620f69bbc31d462435a7431fd7da0ee2549
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2021-10-04 13:41:45 +0200

    feat: rename util

--- package/utils/plain-color.ts
+++ package/utils/plain-color.ts
@@ -1,6 +1,6 @@
 import { Mesh, MeshStandardMaterial } from "three";
 
-export const mutateRandomColor = (mesh: Mesh): void => {
+export const mutatePlainColor = (mesh: Mesh): void => {
     const material = (mesh.material as MeshStandardMaterial).clone();    
     material.color.setHex(Math.random() * 0xffffff);
     mesh.material = material;
