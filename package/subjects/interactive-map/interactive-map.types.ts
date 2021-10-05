d0815bd5a3722c5b71b895d968e483b278054612
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2021-10-05 10:14:28 +0200

    feat: remove old subjects

--- package/subjects/interactive-map/interactive-map.types.ts
+++
@@ -1,9 +0,0 @@
-import { Group } from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-import { IUpdates } from "../../types";
-
-export default interface IInteractiveMap extends IUpdates {
-    loader: GLTFLoader;
-    instance: Group | null;
-}
