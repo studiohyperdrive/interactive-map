5b02abbfe87afa27fa4333b0a8a9fd1f36cf6db3
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2021-10-07 11:35:32 +0200

    chore: remove double types

--- package/plugins/browser-resize-plugin/browser-resize-plugin.types.ts
+++ package/plugins/browser-resize-plugin/browser-resize-plugin.types.ts
@@ -5,7 +5,5 @@ import { IEventPlugin } from "../../types";
 export interface IBrowserResizePlugin extends IEventPlugin {
     renderer: WebGLRenderer,
     camera: PerspectiveCamera | OrthographicCamera,
-    bindEventListener: () => void,
-    unbindEventListener: () => void,
     handleResize: (e:UIEvent) => void,
 }
--- package/plugins/click-plugin/click-plugin.types.ts
+++ package/plugins/click-plugin/click-plugin.types.ts
@@ -5,8 +5,6 @@ import { IAnimate, IBindingConfig, IEventPlugin } from "../../types";
 export interface IClickPlugin extends IEventPlugin {
     animations: AnimationClip[],
     mixer: AnimationMixer,
-    bindEventListener: () => void,
-    unbindEventListener: () => void,
     handleClick: (e: MouseEvent) => void,
     handleBindingAnimation: (binding: IBindingConfig, callback: (animation: AnimationClip, animationBinding: IAnimate) => void) => void,
 }
