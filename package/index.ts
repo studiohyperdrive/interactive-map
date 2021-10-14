17d6ba622a22d95e513b162b2a87f9c31be88e54
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-10-14 11:07:37 +0200

    bug: disable barrel file

--- package/index.ts
+++ package/index.ts
@@ -1,4 +1,6 @@
-export * from "./scene-manager";
-export * from "./three-entry-point";
-export * from "./types";
-export * from "./utils";
+// This barrel file was causing several false-positive "module not found errors"
+
+// export { default as SceneManager } from "./scene-manager";
+// export { default as ThreeEntryPoint } from "./three-entry-point";
+// export * from "./types";
+// export * from "./utils";
