0a340d7758a707970f311b3093438c9e12a0c229
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-10-20 16:09:15 +0200

    feat: Adjust visibility helpers

--- package/utils/visibility.ts
+++ package/utils/visibility.ts
@@ -3,11 +3,11 @@ import { Mesh, Object3D, Vector3 } from "three";
 export const OOO = new Vector3(0, 0, 0);
 
 export const hideChild = (child: Object3D | Mesh): Object3D | Mesh => {
-    return showChild(child, OOO);
+    child.visible = false;
+    return child;
 }
 
-export const showChild = (child: Object3D | Mesh, size: Vector3): Object3D | Mesh => {
-    const { x, y, z } = size;
-    child.scale.set(x, y, z);
+export const showChild = (child: Object3D | Mesh): Object3D | Mesh => {
+    child.visible = true
     return child;
 }
