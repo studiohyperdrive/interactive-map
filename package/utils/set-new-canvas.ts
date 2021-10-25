c9a32853e91bda499949690a61e0acf875fe1f71
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-10-25 09:45:44 +0200

    feat: add docs

--- package/utils/set-new-canvas.ts
+++ package/utils/set-new-canvas.ts
@@ -5,14 +5,22 @@ import { IMapControlsPlugin, IWebglRendererPlugin } from "../plugins";
 import { getControlsPlugin, rebuildMapControlsPlugin } from "./manage-controls";
 import { getRendererPlugin, rebuildRendererPlugin } from "./manage-renderer";
 
+/**
+ * Helper function to replace the canvas instance on a ThreeEntryPoint instance.
+ * 
+ * ⚠️ **Note:** This function can not be used together with CameraLerpPlugin as it breaks smooth pann- & zooming.
+ * 
+ * @param three The current ThreeEntryPoint instance that should receive a new target canvas
+ * @param canvas A reference to an existing canvas element
+ */
 export const setNewCanvas = (three: ThreeEntryPoint, canvas: HTMLCanvasElement) => {
     const rendererPlugin: IWebglRendererPlugin | undefined = getRendererPlugin(three);
     if (rendererPlugin) {
         rebuildRendererPlugin(rendererPlugin, canvas, rendererPlugin.sizes, rendererPlugin.config);
     }
 
-    // const controlsPlugin: IMapControlsPlugin | undefined = getControlsPlugin(three);
-    // if (controlsPlugin) {
-    //     rebuildMapControlsPlugin(controlsPlugin, controlsPlugin.camera, canvas);
-    // }
+    const controlsPlugin: IMapControlsPlugin | undefined = getControlsPlugin(three);
+    if (controlsPlugin) {
+        rebuildMapControlsPlugin(controlsPlugin, controlsPlugin.camera, canvas);
+    }
 }
