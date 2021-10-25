fb1286a9beb1fedecabb883c97cb3a969630f757
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-10-25 11:18:33 +0200

    feat: create zoomToConfig util

--- package/utils/zoom.ts
+++ package/utils/zoom.ts
@@ -1,5 +1,6 @@
 import { Box3, Mesh, Object3D, OrthographicCamera, PerspectiveCamera, Vector3 } from "three";
 import { MapControls } from "three/examples/jsm/controls/OrbitControls";
+import { IOrthographicCameraConfig, IPerspectiveCameraConfig } from "../types";
 
 /**
  * This function will adjust the camera and controls to zoom in and center on a set of objects.
@@ -10,7 +11,13 @@ import { MapControls } from "three/examples/jsm/controls/OrbitControls";
  * @param setZoomProps A function exposed by the TabNavigationPlugin that updates the zoomProps value in the package's internal Datastore
  * @param fitRatio A number dictating how much "padding" should be around the zoomed-to object
  */
-export const zoomCameraToSelection = (camera: PerspectiveCamera | OrthographicCamera, controls: MapControls, selection: Array<Object3D | Mesh>, setZoomProps: (props: any) => void, fitRatio = 1.2) => {
+export const zoomCameraToSelection = (
+  camera: PerspectiveCamera | OrthographicCamera,
+  controls: MapControls,
+  selection: Array<Object3D | Mesh>,
+  setZoomProps: (props: any) => void,
+  fitRatio = 1.2
+) => {
   // Create bounding box for selection  
   const box = new Box3();
 
@@ -44,7 +51,7 @@ export const zoomCameraToSelection = (camera: PerspectiveCamera | OrthographicCa
       .multiplyScalar(distance);
   }
 
-  // Set zoomProps for transition in tab-navigation-transition-plugin
+  // Set zoomProps for transition in camera-lerp-plugin
   setZoomProps({
     boundingBox: box,
     aspect: aspect,
@@ -53,7 +60,55 @@ export const zoomCameraToSelection = (camera: PerspectiveCamera | OrthographicCa
   });
 }
 
-const getAspectRatioFromControls = (controls: MapControls): number => {
+export const setCameraToConfig = (
+  camera: PerspectiveCamera | OrthographicCamera,
+  controls: MapControls,
+  config: IOrthographicCameraConfig | IPerspectiveCameraConfig,
+  setZoomProps: (props: any) => void,
+) => {
+  const ratio = getAspectRatioFromControls(controls);
+
+  // Stop any animations
+  setZoomProps(undefined);
+
+  // Coordinate 0, 0, 0
+  const OOO = new Vector3;
+
+  camera.position.set(config.position.x, config.position.y, config.position.z);
+  camera.lookAt(OOO);
+
+  controls.target.set(OOO.x, OOO.y, OOO.z);
+
+  // instanceof operator does not work here
+  switch (camera.type) {
+    case "OrthographicCamera":
+      const orthoConfig = config as IOrthographicCameraConfig;
+
+      camera.left = orthoConfig.frustumSize * ratio / - 2;
+      camera.right = orthoConfig.frustumSize * ratio / 2;
+      camera.top = orthoConfig.frustumSize / 2;
+      camera.bottom = orthoConfig.frustumSize / - 2;
+      break;
+
+    case "PerspectiveCamera":
+      const perspectiveConfig = config as PerspectiveCamera;
+
+      camera.fov = perspectiveConfig.fov;
+      camera.aspect = ratio;
+      break;
+  
+    default:
+      break;
+  }
+
+  camera.near = config.near;
+  camera.far = config.far;
+
+  controls.update();
+  camera.updateProjectionMatrix();
+}
+
+export const getAspectRatioFromControls = (controls: MapControls): number => {
   // Determine aspect ratio
 
   let container = undefined;
