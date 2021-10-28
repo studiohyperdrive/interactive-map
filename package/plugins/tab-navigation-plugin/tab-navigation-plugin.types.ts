cb21522497e3d3f53aa5ec77ed257068fb4deab8
Ian Emsens <ian.emsens@studiohyperdrive.be>
2021-10-28 12:40:42 +0200

    feat: expand tab plugin typing

--- package/plugins/tab-navigation-plugin/tab-navigation-plugin.types.ts
+++ package/plugins/tab-navigation-plugin/tab-navigation-plugin.types.ts
@@ -1,6 +1,19 @@
+import { AnimationClip, AnimationMixer, Scene } from "three";
+
 import { IEventPlugin, IBindingConfig, BindingCallback } from "../../types";
 
 export interface ITabNavigationPlugin extends IEventPlugin {
+    scene: Scene;
+    animations: AnimationClip[];
+    bindings: ITabNavigationBindingConfig[];
+    current?: ITabNavigationBindingConfig;
+    mixer: AnimationMixer;
+
+    listeners: {
+        tab: EventListener,
+        shiftTab: EventListener
+    }
+
     handleTabPress(e: KeyboardEvent): void
     handleShiftTabPress(e: KeyboardEvent): void
     navigate(e: KeyboardEvent, forward: boolean): void
